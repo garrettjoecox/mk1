4f537966f4ae20841a9e361b58c4e3d9c8c6a48a
Garrett Cox <garrettjcox@gmail.com>
2020-03-13 19:36:04 -0500

    deploy

---
+++ precache-manifest.73c40157f33bab18494bd301e7bdefa8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fbe92aaa423915fd0e",
+    "url": "/mk1/css/app.bb4ad4a1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/mk1/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0634286928b8d83e38e3492f251261e8",
+    "url": "/mk1/index.html"
+  },
+  {
+    "revision": "294f5f199927e7147d15",
+    "url": "/mk1/js/about.bb8cdee2.js"
+  },
+  {
+    "revision": "b4fbe92aaa423915fd0e",
+    "url": "/mk1/js/app.a14c36ac.js"
+  },
+  {
+    "revision": "eb5c47a42c44bfee4a6f",
+    "url": "/mk1/js/chunk-vendors.816d8bd0.js"
+  },
+  {
+    "revision": "4ce26cebbe19a996725699b6549735c5",
+    "url": "/mk1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mk1/robots.txt"
+  }
+]);
